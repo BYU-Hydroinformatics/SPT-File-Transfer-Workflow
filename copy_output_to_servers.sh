251e3ede7639a3688eba2dde371cb5de790d2507
Michael <msouff@byu.edu>
2017-07-17 10:36:07 -0600

    added functionality to delete dates older than two weeks

--- copy_output_to_servers.sh
+++ copy_output_to_servers.sh
@@ -7,7 +7,3 @@ ssh -n $TETHYS_SERVER "mkdir -p $TETHYS_PATH"
 # syncs results between servers
 echo Copying $1/$2 to $TETHYS_SERVER:$TETHYS_PATH/$3
 rsync --ignore-existing ~/rapid-io/output/$1/$2/* $TETHYS_SERVER:$TETHYS_PATH/$3/
-
-# delete results once copied
-#echo Deleting local version of /$1/$2
-#rm -r ~/rapid-io/output/$1/$2
--- workflow.sh
+++ workflow.sh
@@ -16,7 +16,11 @@ DATE=$(date +"%Y%m%d.%H%M%S")
 TODAY=$(date +"%Y%m%d")
 DATE_LIMIT=$(date -d "$TODAY - 14 days" +"%Y%m%d")
 LOG=$WORKFLOW_DIR/logs/workflow_$DATE.log
-echo Initializing log > $LOG
+echo Initializing log >> $LOG
+
+# run the ecmwf/rapid process
+echo "Running RAPID process (see details at ~/logs)" >> $LOG
+python $HOME/scripts/run.py
 
 # copy output files to SPT server
 while read watershed
@@ -42,13 +46,11 @@ do
         . $WORKFLOW_DIR/copy_output_to_servers.sh $watershed $DATE_LOCAL_FORMAT $DATE_EXT_FORMAT >> $LOG
       else
         echo "Skipping available date older than two weeks" >> $LOG
+#        echo "Deleting dates older than two weeks" >> $LOG
+#        rm -r $HOME/rapid-io/output/$watershed/$rawdate
       fi
     done < <(ls -d $HOME/rapid-io/output/$watershed/*/ | xargs -n 1 basename)
   else
     echo $watershed" directory is empty" >> $LOG
   fi
 done < <(ls -d $HOME/rapid-io/input/*/ | xargs -n 1 basename)
-
-# run the ecmwf/rapid process
-echo "Running RAPID process (see details at ~/logs)" >> $LOG
-python $HOME/scripts/run.py
